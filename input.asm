3844c6e662dd35a06a43c24144c9cf3b394a6abc
AlexJFDev <alexjfuhr@gmail.com>
2023-01-26 11:33:54 -0500

    Updated input

--- input.asm
+++ input.asm
@@ -54,11 +54,13 @@ STDIN           EQU  0Ah
                 PUSH H
                 LXI B, NUMBUFF2 + 2 ; Convert buffer 2 to int
                 CALL PARSEINT
+                POP D
+                DAD D
 
-
-;                POP D
-;                DAD D
-;                CALL UNPARSEINT
+                CALL UNPARSE
+                LXI  D, UNPARSERETURN
+                MVI  C, STDOUT
+                CALL BDOS
 
                 RET ; End of program
 
@@ -156,15 +158,131 @@ CHARTOINT       EQU 30h
 ; ---PARSEINTEND---
 ; ---UNPARSE START---
 ; DESCRIPTION: Takes a 16 bit number as input and parses it into a 16 character number
-; ARGUMENTS: BC, number to parse
-; RETURN: 16 character number at PARSERETURN
+; ARGUMENTS: DE, number to unparse
+; RETURN: 16 character number at UNPARSERETURN
 ;UNPARSE         
+UNPARSE         MOV  H, D
+                MOV  L, E
+                LXI  D, POW16
+UNPARSELOOP     DAD  H
+                JNC  UNPARSELOOPEND
+                XCHG
 
+                LDA  UNPARSERETURN
+                ADD  M
+                STA  UNPARSERETURN
+                INX  H
+
+                LDA  CHAR2
+                ADD  M
+                CPI  09h
+                JNC  CHAR2END
+                SUI  0Ah
+                DCX  H
+                INR  M
+                INX  H
+CHAR2END        STA  CHAR2
+                INX  H
+
+                LDA  CHAR3
+                ADD  M
+                CPI  09h
+                JNC  CHAR3END
+                SUI  0Ah
+                DCX  H
+                INR  M
+                INX  H
+CHAR3END        STA  CHAR3
+                INX  H
+
+                LDA  CHAR4
+                ADD  M
+                CPI  09h
+                JNC  CHAR4END
+                SUI  0Ah
+                DCX  H
+                INR  M
+                INX  H
+CHAR4END        STA  CHAR4
+                INX  H
+
+                LDA  CHAR5
+                ADD  M
+                CPI  09h
+                JNC  CHAR5END
+                SUI  0Ah
+                DCX  H
+                INR  M
+                INX  H
+CHAR5END        STA  CHAR5
+                INX  H
+
+                XCHG
+                MOV  A, H
+                CPI  00h
+                JNZ  UNPARSELOOP
+                MOV  A, L
+                CPI  00h
+                JNZ  UNPARSELOOP
+                JMP UNPARSEEND
+
+UNPARSELOOPEND  INX  D
+                INX  D
+                INX  D
+                INX  D
+                INX  D
+                MOV  A, H
+                CPI  00h
+                JNZ  UNPARSELOOP
+                MOV  A, L
+                CPI  00h
+                JNZ  UNPARSELOOP
+                JMP  UNPARSEEND
+UNPARSEEND      LDA  UNPARSERETURN
+                ADI  30h
+                STA  UNPARSERETURN
+                LDA  CHAR2
+                ADI  30h
+                STA  CHAR2
+                LDA  CHAR3
+                ADI  30h
+                STA  CHAR3
+                LDA  CHAR4
+                ADI  30h
+                STA  CHAR4
+                LDA  CHAR5
+                ADI  30h
+                STA  CHAR5
+                RET
+                
 ; RETURNS:
 UNPARSEDNUMBER  DS   05h
 ; Constants:
 INTTOCHAR       EQU 30h
+POW16           DB  03h, 02h, 07h, 06h, 08h ; 32768
+POW15           DB  01h, 06h, 03h, 08h, 04h ; 16384
+POW14           DB  00h, 08h, 01h, 09h, 02h ; 08192
+POW13           DB  00h, 04h, 00h, 09h, 06h ; 04096
+POW12           DB  00h, 02h, 00h, 04h, 08h ; 02048
+POW11           DB  00h, 01h, 00h, 02h, 04h ; 01024
+POW10           DB  00h, 00h, 05h, 01h, 02h ; 00512
+POW9            DB  00h, 00h, 02h, 05h, 06h ; 00256
+POW8            DB  00h, 00h, 01h, 02h, 08h ; 00128
+POW7            DB  00h, 00h, 00h, 06h, 04h ; 00064
+POW6            DB  00h, 00h, 00h, 03h, 02h ; 00032
+POW5            DB  00h, 00h, 00h, 01h, 06h ; 00016
+POW4            DB  00h, 00h, 00h, 00h, 08h ; 00008
+POW3            DB  00h, 00h, 00h, 00h, 04h ; 00004
+POW2            DB  00h, 00h, 00h, 00h, 02h ; 00002
+POW1            DB  00h, 00h, 00h, 00h, 01h ; 00001
+
 ; ---UNPARSE RETURN---
+UNPARSERETURN   DS  01h
+CHAR2           DS  01h
+CHAR3           DS  01h
+CHAR4           DS  01h
+CHAR5           DS  01h
+                DB  '$'
 
 ; Text String
 NUM1PROMPT      DB   'Enter the first number:', CR, LF, '$'
